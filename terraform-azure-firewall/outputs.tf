5fb7749db40f02c4f5c301b96e4983475fe35bb0
naveenSprint <muppala.devops@gmail.com>
2023-08-16 16:08:25 +0530

    Create outputs.tf

---
+++ terraform-azure-firewall/outputs.tf
@@ -0,0 +1,19 @@
+output "firewall_id" {
+  description = "Firewall generated id"
+  value       = azurerm_firewall.firewall[*].id
+}
+
+output "firewall_name" {
+  description = "Firewall name"
+  value       = azurerm_firewall.firewall[*].name
+}
+
+output "private_ip_address" {
+  description = "Firewall private IP"
+  value       = azurerm_firewall.firewall[*].ip_configuration[0].private_ip_address
+}
+
+output "public_ip_address" {
+  description = "Firewall public IP"
+  value       = azurerm_public_ip.firewall_public_ip[*].ip_address
+}
